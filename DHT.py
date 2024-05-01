e8aba4320bdd3e99bc130d9a78676c23ae1e3fcb
Daffa Asha <80553712+daffaasha@users.noreply.github.com>
2024-05-01 22:25:30 +0700

    Create DHT.py

---
+++ DHT.py
@@ -0,0 +1,9 @@
+//DHT
+
+import Adafruit_DHT
+
+sensor = Adafruit_DHT.DHT22
+ 
+while True:
+   humidity, temperature = Adafruit_DHT.read_retry(22, 4)
+   print ('Temp: {0:0.1f} C  Humidity: {1:0.1f} %'.format(temperature, humidity))
